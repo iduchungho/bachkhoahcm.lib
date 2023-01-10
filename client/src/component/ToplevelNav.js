a496bfb09638801249ba323fcce062fcedaedfd8
Kawasaki <iduchungho@gmail.com>
2023-01-10 20:45:01 +0700

    add login box

--- client/src/component/ToplevelNav.js
+++ client/src/component/ToplevelNav.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
 import './styles/ToplevelNav.css';
 import { BiSearch } from 'react-icons/bi';
-import { Button, IconButton, Modal, Placeholder } from "rsuite";
+import { Avatar, Button, Divider, Form, IconButton, Modal, Placeholder } from "rsuite";
 import { FiBookOpen } from 'react-icons/fi';
-import { AiFillGithub } from 'react-icons/ai';
-import { FaDiscord } from 'react-icons/fa';
-import { BsFacebook } from 'react-icons/bs';
+// import { AiFillGithub } from 'react-icons/ai';
+// import { FaDiscord } from 'react-icons/fa';
+// import { BsFacebook } from 'react-icons/bs';
 import { TiDocumentText } from 'react-icons/ti';
 import { BiNews } from 'react-icons/bi';
 import bkicon from '../img/favicon.ico';
-import { FACEBOOK, GITHUB } from "./utils/link.utils";
+// import { FACEBOOK, GITHUB } from "./utils/link.utils";
+import UserIcon from '@rsuite/icons/legacy/User';
 
 function ToplevelNav() {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const [openLogin, setOpenLogin] = useState(false);
+    const handleOpenLogin = () => setOpenLogin(true);
+    const handleCloseLogin = () => setOpenLogin(false);
+
+    const LoginOrUser = (props) => {
+        if (props === true)
+            return (
+                <div className="login-btn">
+                    <Button appearance="primary" size="sm" onClick={handleOpenLogin}>Đăng nhập</Button>
+                </div>
+            )
+        else
+            return (<Avatar circle className="user-avt" size="md"><UserIcon className="user-ico" /></Avatar>)
+    }
+
     return (
         <div className='top-level-nav'>
             <a href="/">
@@ -25,15 +41,16 @@ function ToplevelNav() {
                 />
             </a>
             <div className='top-level-nav-header'>
-                <IconButton icon={<BiSearch />} appearance="subtle" circle className="ico-btn" onClick={handleOpen}/>
-                <IconButton icon={<FiBookOpen />} appearance="subtle" circle className="ico-btn" href="/aboutus"/>
+                <IconButton icon={<BiSearch />} appearance="subtle" circle className="ico-btn" onClick={handleOpen} />
+                <IconButton icon={<FiBookOpen />} appearance="subtle" circle className="ico-btn" href="/aboutus" />
                 <IconButton icon={<TiDocumentText />} appearance="subtle" circle className="ico-btn" href="/doc" />
-                <IconButton icon={<BiNews/>} appearance="subtle" circle className="ico-btn" href="/news" />
-                <IconButton icon={<AiFillGithub />} appearance="subtle" circle className="ico-btn" href={GITHUB}/>
+                <IconButton icon={<BiNews />} appearance="subtle" circle className="ico-btn" href="/news" />
+                {/* <IconButton icon={<AiFillGithub />} appearance="subtle" circle className="ico-btn" href={GITHUB}/>
                 <IconButton icon={<FaDiscord />} appearance="subtle" circle className="ico-btn" />
-                <IconButton icon={<BsFacebook />} appearance="subtle" circle className="ico-btn" href={FACEBOOK}/>
+                <IconButton icon={<BsFacebook />} appearance="subtle" circle className="ico-btn" href={FACEBOOK}/> */}
             </div>
 
+            {/* Search box */}
             <Modal open={open} onClose={handleClose}>
                 <Modal.Header>
                     <Modal.Title>Search</Modal.Title>
@@ -50,6 +67,39 @@ function ToplevelNav() {
                     </Button>
                 </Modal.Footer>
             </Modal>
+
+            {   //set false when user clicks on login button
+                LoginOrUser(true)
+            }
+
+            {/* Login box */}
+            <Modal open={openLogin} onClose={handleCloseLogin} className="login-form">
+                <Modal.Header>
+                    <Modal.Title></Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form fluid>
+                        <div className="sp-title">Đăng nhập</div>
+                        <div className="sp-sub-title">Nhanh chóng và dễ dàng</div>
+                        <Divider />
+                        <Form.Group controlId="email">
+                            <Form.ControlLabel>Email</Form.ControlLabel>
+                            <Form.Control name="email" placeholder="Email" />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.ControlLabel>Password</Form.ControlLabel>
+                            <Form.Control name="password" type="password" autoComplete="off" placeholder="Password" />
+                        </Form.Group>
+                        <Button appearance="primary" className="login-btn2">Đăng nhập</Button>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <a href="/">Quên mật khẩu</a>
+                    <Divider />
+                    <Button color="green" appearance="primary" className="create-account">Tạo tài khoản mới</Button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
     );
 }
